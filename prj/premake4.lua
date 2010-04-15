415977060c1c86dc20a56d78bf7ca7e1031f2f8a
danomatika <danomatika@gmail.com>
2010-04-15 00:06:04 +0000

    added premake4 build script and xcode projects; removed old premake script, codeblocks projects, and old xcode project

---
+++ prj/premake4.lua
@@ -0,0 +1,82 @@
+--[[ A solution contains projects, and defines the available configurations
+
+http://industriousone.com/premake/user-guide
+
+example: http://opende.svn.sourceforge.net/viewvc/opende/trunk/build/premake4.lua?revision=1708&view=markup
+
+http://bitbucket.org/anders/lightweight/src/tip/premake4.lua
+
+]]
+solution "visualframework"
+	configurations { "Debug", "Release" }
+	objdir "../obj"
+ 
+-- convenience library
+project "visualframework"
+	kind "StaticLib"
+	language "C++"
+	targetdir "../lib"
+	files { "../src/visualframework/**.h", "../src/visualframework/**.cpp" }
+	
+	includedirs { "../src", "../externals" }
+	libdirs {  }
+	
+	configuration "linux"
+		buildoptions { "`pkg-config --cflags sdl`",
+					   "`pkg-config --cflags SDL_gfx`",
+					   "`pkg-config --cflags SDL_image`" }
+		linkoptions { "`pkg-config --libs sdl`",
+					  "`pkg-config --libs SDL_gfx`",
+					  "`pkg-config --cflags SDL_image`" }
+	
+	configuration "macosx"
+		-- MacPorts
+		includedirs { "/opt/local/include" }
+		libdirs { "/opt/local/lib" }
+
+	configuration "Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+
+	configuration "Release"
+		defines { "NDEBUG" }
+		flags { "Optimize" } 
+
+-- visual test
+project "vftest"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir "../bin"
+	files { "../src/test/**.h", "../src/test/**.cpp" }
+	
+	includedirs { "../src", "../src/visualframework",
+				  "../externals" }
+	libdirs { }
+	links { "visualframework" }
+
+	configuration "linux"
+		buildoptions { "`pkg-config --cflags sdl`",
+					   "`pkg-config --cflags SDL_gfx`",
+					   "`pkg-config --cflags SDL_image`" }
+		linkoptions { "`pkg-config --libs sdl`",
+					  "`pkg-config --libs SDL_gfx`",
+					  "`pkg-config --cflags SDL_image`" }
+
+	configuration "macosx"
+		-- MacPorts
+		includedirs { "/opt/local/include"}
+		libdirs { "/opt/local/lib" }
+		links { "lo", "pthread", "SDLmain", "SDL", "SDL_gfx", "SDL_image", "SDL_net" }
+		linkoptions { "-Wl,-framework,Cocoa", "-Wl,-framework,OpenGL",
+					  "-Wl,-framework,ApplicationServices",
+					  "-Wl,-framework,Carbon", "-Wl,-framework,AudioToolbox",
+					  "-Wl,-framework,AudioUnit", "-Wl,-framework,IOKit" }
+
+	configuration "Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+
+	configuration "Release"
+		defines { "NDEBUG" }
+		flags { "Optimize" }
+		
--- prj/xcode/visualframework/build/visualframework.build/Debug/configure.build/Script-3056617E10F55DD9007A4B62.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# export the dirs for MacPorts
-export CPPFLAGS=-I/opt/local/include
-export LDFLAGS=-L/opt/local/lib
-
-# run the makefile
-make -f Makefile-xcode $@
-
-exit 0
